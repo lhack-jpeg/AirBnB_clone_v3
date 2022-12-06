0fb95739f42d4b687814078b8cdeb09884a1dfaf
Luke Hackett <luke-hackett@outlook.com>
2022-12-06 15:59:15 +1100

    add routes for delete, post, put

--- api/v1/views/state.py
+++ api/v1/views/state.py
@@ -5,7 +5,7 @@ state obj.
 '''
 
 from api.v1.views import app_views
-from flask import jsonify, abort
+from flask import jsonify, abort, Response, request
 from models import storage
 from models.state import State
 
@@ -30,3 +30,58 @@ def get_one_state(state_id):
 
     single_state_dict = result.to_dict()
     return jsonify(single_state_dict)
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete_one_state(state_id):
+    '''Checks to see if state exists before deleting from db'''
+    result = storage.get(State, state_id)
+    if result is None:
+        abort(404)
+
+    storage.delete(result)
+    return Response({}, status=200)
+
+
+@app_views.route('/states', methods=['POST'])
+def create_state():
+    '''Creates a new state from the http body'''
+    new_obj = request.get_json()
+    '''If not json abort to 400 code'''
+    if new_obj is None:
+        abort(400, 'Not a JSON')
+
+    '''If obj doesn't have key of name, abort 400'''
+    if new_obj.get('name') is None:
+        abort(400, 'Missing name')
+
+    '''Create new obj, save to storage and return json obj.'''
+    new_state = State(**new_obj)
+    storage.new(new_state)
+    storage.save(new_state)
+    return jsonify(new_state.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['POST'])
+def update_state(state_id):
+    '''
+    Updates state by id. Checks to make sure if it exists.
+    Sends either 404 if states doesn't exists.
+    Sends 400 if json in http body is bad.
+    Ignores keys id, updated_at, created_at.
+    '''
+    new_attributes = request.get_json()
+    if new_attributes is None:
+        abort(400, 'Not a JSON')
+
+    one_state = storage.get(State, state_id)
+    if one_state is None:
+        abort(404)
+
+    skip_keys = ['id', 'created_at', 'updated_at']
+    for key, value in new_attributes:
+        if key in skip_keys:
+            pass
+        else:
+            one_state[key] = value
+    return Response(jsonify(one_state.to_dict()), 200)
