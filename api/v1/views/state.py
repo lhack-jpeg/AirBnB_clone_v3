09d3f7fca15640784b9b78680bbf811f0d102d8d
Luke Hackett <luke-hackett@outlook.com>
2022-12-06 15:15:57 +1100

    add state_id route

--- api/v1/views/state.py
+++ api/v1/views/state.py
@@ -10,12 +10,21 @@ from models import storage
 from models.state import State
 
 
-@app_views.route('/states', strict_slashes=False)
+@app_views.route('/states', method='GET')
 def get_all_states():
     '''Returns the json object of all states in storage.'''
     state_list = []
     all_states = storage.all('State')
     for _state in all_states.values():
-        state_list.append(_state.to_dict()) 
-       
+        state_list.append(_state.to_dict())
+
     return jsonify(state_list)
+
+
+@app_views.route('/states/<state_id>', method='GET')
+def get_one_state(state_id):
+    '''Returns the one state in json or else the 404 page.'''
+    result = storage.get(State, state_id)
+    print(result)
+
+    return jsonify({'status': 200})
